def7c4a9287ed4061697c1ff62f786118b7a5e6e
karthik-tarento <karthikeyan.rajendran@tarento.com>
2025-02-28 20:10:49 +0530

    Added logs

--- keycloak/sms-provider/src/main/java/org/sunbird/keycloak/login/PasswordAndOtpAuthenticator.java
+++ keycloak/sms-provider/src/main/java/org/sunbird/keycloak/login/PasswordAndOtpAuthenticator.java
@@ -507,7 +507,7 @@ public class PasswordAndOtpAuthenticator extends AbstractUsernameFormAuthenticat
 
 		String storedCode = context.getAuthenticationSession().getAuthNote(Constants.SESSION_OTP_CODE);
 		logger.info("Form Data");
-		logger.info("Entered Code: %s, Stored Code %s",enteredCode, storedCode);
+		logger.info(String.format("Entered Code: %s, Stored Code %s",enteredCode, storedCode));
 		if (storedCode != null && enteredCode != null) {
 			result = storedCode.equalsIgnoreCase(enteredCode) ? CODE_STATUS.VALID : CODE_STATUS.INVALID;
 		}
