e04d744eb4fc558d329f0de64a406fa83f2bfe8a
vishnu <vishnubansaltarento>
2026-04-23 11:26:49 +0530

    update for ai assessment

--- keycloak/sms-provider/src/main/java/org/sunbird/keycloak/login/PasswordAndOtpAuthenticator.java
+++ keycloak/sms-provider/src/main/java/org/sunbird/keycloak/login/PasswordAndOtpAuthenticator.java
@@ -87,7 +87,10 @@ public class PasswordAndOtpAuthenticator extends AbstractUsernameFormAuthenticat
 		if (context.getAuthenticationSession().getRedirectUri().contains(Constants.EC_LOGIN)) {
 			context.getAuthenticationSession().setAuthNote(Constants.AUTH_NOTE_LOGIN_PAGE, Constants.EC_LOGIN_PAGE);
 			context.challenge(formsProvider.createForm(Constants.EC_LOGIN_PAGE));
-		} else {
+		} else if (context.getAuthenticationSession().getRedirectUri().contains(Constants.AI_ASSESSMENT_LOGIN_PAGE)) {
+			context.getAuthenticationSession().setAuthNote(Constants.AUTH_NOTE_LOGIN_PAGE, Constants.AI_ASSESSMENT_LOGIN_PAGE);
+			context.challenge(formsProvider.createForm(Constants.AI_ASSESSMENT_LOGIN_PAGE));
+		}  else {
 			context.challenge(formsProvider.createForm(Constants.LOGIN_PAGE));
 		}
 	}
